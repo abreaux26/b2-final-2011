a404dbe9c41fed460b158f550c58dec1a24c6dd8
abreaux26 <angelbreaux26@gmail.com>
2021-03-05 10:25:21 -0600

    Create spec flight class method to order by departure city

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Flight do
     it { should have_many :flight_passengers }
     it { should have_many(:passengers).through(:flight_passengers) }
   end
+
+  before :each do
+    @flight_1 = Flight.create!(number: '1727', date:  '08/03/20', departure_city: 'Denver', arrival_city: 'Reno')
+    @flight_2 = Flight.create!(number: '1562', date:  '08/05/20', departure_city: 'Conway', arrival_city: 'Houston')
+  end
+
+  describe 'class methods' do
+    describe '::order_by_departure_city' do
+      it 'returns flights in alphabetical order by departure city' do
+        expected = [@flight_2, @flight_1]
+
+        expect(Flight.order_by_departure_city).to eq(expected)
+      end
+    end
+  end
 end
