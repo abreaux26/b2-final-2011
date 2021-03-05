bb0c24fe964f8d1063534be97127d2b612210aed
abreaux26 <angelbreaux26@gmail.com>
2021-03-05 12:10:58 -0600

    Update extension to have edge case if there are no passengers present, then order by departure city

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,7 +3,7 @@ class Flight < ApplicationRecord
   has_many :passengers, through: :flight_passengers
 
   def self.order_by_passenger_count_departure_city
-    joins(:passengers)
+    left_joins(:passengers)
     .select('flights.*, count(passengers.id) as passenger_count')
     .group(:id)
     .order('passenger_count desc, departure_city')
