22641f0c9f091730d0b7829d9f3ecb5fb765518d
abreaux26 <angelbreaux26@gmail.com>
2021-03-05 11:00:04 -0600

    Refactor models to move adult passenger methods to passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,12 @@
 class Passenger < ApplicationRecord
   has_many :flight_passengers
   has_many :flights, through: :flight_passengers
+
+  def self.adults
+    where('age >= 18')
+  end
+
+  def self.adults_avg_age
+    adults.average(:age).to_f
+  end
 end
