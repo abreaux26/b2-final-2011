712e966498e5253d19f9525f567503f82f8a075e
abreaux26 <angelbreaux26@gmail.com>
2021-03-05 10:59:38 -0600

    Refactor spec to move adult passenger methods to passenger model

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe Passenger do
     it { should have_many :flight_passengers }
     it { should have_many(:flights).through(:flight_passengers) }
   end
+
+  before :each do
+    @passenger_1 = Passenger.create!(name: 'Angel', age: 24)
+    @passenger_2 = Passenger.create!(name: 'Chris', age: 28)
+    @passenger_3 = Passenger.create!(name: 'Easton', age: 8)
+  end
+
+  describe 'class methods' do
+    describe '::adults' do
+      it 'returns passengers whose age is greater than or equal to 18' do
+        expected = [@passenger_1, @passenger_2]
+
+        expect(Passenger.adults).to eq(expected)
+      end
+    end
+    describe '::adults_avg_age' do
+      it 'returns adult passengers average age' do
+        expect(Passenger.adults_avg_age).to eq(26)
+      end
+    end
+  end
 end
